9e47a38b553ebe73c5bbe8378e71652474bd1ac3
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 17:34:18 +0300

    Update tech09-0-count-sigint.c

--- signals/tech09-0-count-sigint.c
+++ signals/tech09-0-count-sigint.c
@@ -1,10 +1,20 @@
-/*
+/* RU
 
 Программа при запуске сообщает на стандартный поток вывода свой PID, выталкивает буфер вывода с помощью fflush, после чего начинает обрабатывать поступающие сигналы.
 
 При поступлении сигнала SIGTERM необходимо вывести на стандартный поток вывода целое число: количество ранее поступивших сигналов SIGINT и завершить свою работу.
 
 Семантика повединия сигналов (Sys-V или BSD) считается не определенной.
+
+*/
+
+/* ENG
+
+At startup, the program reports its PID to the standard output stream, pushes the output buffer using fflush, and then begins processing incoming signals.
+
+When a SIGTERM signal is received, it is necessary to output an integer to the standard output stream: the number of previously received SIGINT signals and complete its work.
+
+The semantics of behavioral signals (Sys-V or BSD) is considered undefined.
 
 */
 
