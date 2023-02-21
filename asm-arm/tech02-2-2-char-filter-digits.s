c090e65c4822a585a23aa18ae8311e878fe403e2
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 17:21:43 +0300

    Update tech02-2-2-char-filter-digits.s

--- asm-arm/tech02-2-2-char-filter-digits.s
+++ asm-arm/tech02-2-2-char-filter-digits.s
@@ -1,10 +1,20 @@
-/*
+/* RU
 
 Напишите программу на языке ассемблера armv8 (AArch64), которая читает со стандартного потока ввода последовательность символов, и выводит только те из них, которые являются десятичными цифрами от 0 до 9.
 
 Можно использовать функции стандартной библиотеки языка Си: getchar_unlocked и putchar_unclocked.
 
 В качестве точки входа можно считать функцию main.
+
+*/
+
+/* ENG
+
+Write a program in the assembly language armv8 (AArch64), which reads a sequence of characters from the standard input stream, and outputs only those of them that are decimal digits from 0 to 9.
+
+You can use the functions of the standard C library: getchar_unlocked and putchar_unclocked.
+
+The main function can be considered as an entry point.
 
 */
 
