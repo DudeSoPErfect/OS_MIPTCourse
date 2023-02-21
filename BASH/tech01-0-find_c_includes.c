bde775268f31c08aafd2895dff2e27191bb2ba6d
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 17:15:22 +0300

    Update tech01-0-find_c_includes.c

--- BASH/tech01-0-find_c_includes.c
+++ BASH/tech01-0-find_c_includes.c
@@ -1,4 +1,4 @@
-/*
+/* RU
 Реализуйте скрипт для командного интерпретатора bash, который выполняет поиск имен #include-файлов для заданных имен Си-функций, используя в качестве источника данных третий раздел man-страниц Linux.
 
 В переменной окружения MANPATH перечислены директории, разделённые символом : (аналогично переменной PATH). В этих директориях располагается каталог man3, устроенный аналогично /usr/share/man/man3. Ответ должен быть составлен с использованием этих директорий, использование man и manpath запрещено.
@@ -25,6 +25,32 @@ string.h
 
 */
 
+/* ENG
+Implement a script for the bash command interpreter that searches for the names of #include files for the given names of C functions using the third section of the Linux man pages as a data source.
+
+The MANPATH environment variable lists directories separated by : (similar to the PATH variable). These directories contain the man3 directory, arranged similarly to /usr/share/man/man3. The response must be compiled using these directories, the use of man and manpath is prohibited.
+
+Files can be either compressed by the gz utility or decompressed.
+
+The list of functions is set on the standard input stream:
+
+fopen
+malloc
+unknown_function
+strcmp
+If the function description is not found, then a dash of three hyphens is written: ---.
+
+The result must be written to the standard output stream:
+
+stdio.h
+stdlib.h
+---
+string.h
+Only input/output commands and sed and gunzip utilities are allowed in the script.
+
+Use a call to interpreters of high-level languages (Perl, Python, etc.).
+
+*/
 
 #!/bin/bash
 
