90651044565cbeda09a2f82f473fb816171d2b11
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 17:16:18 +0300

    Update tech02-1-0-expression-64.s

--- asm-arm/tech02-1-0-expression-64.s
+++ asm-arm/tech02-1-0-expression-64.s
@@ -1,4 +1,4 @@
-/*
+/* RU
 Реализуйте на языке ассемблера armv8 (AArch64) функцию с меткой f, которая вычисляет значение выражения y=Ax2+Bx+C
 
 Значения A, B, C и x хранятся, соответсвенно, в регистрах x0, x1, x2 и x3.
@@ -10,6 +10,20 @@
 Использовать оперативную память запрещено, - можно использовать только регистры.
 
 */
+
+/* ENG
+Implement in the assembly language armv8 (AArch64) a function with the label f, which calculates the value of the expression y=Ax2+Bx+C
+
+The values A, B, C and x are stored, respectively, in registers x0, x1, x2 and x3.
+
+Save the result of the expression calculation in the x0 register.
+
+All arguments and the value of the expression are represented by 64-bit signed integers.
+
+It is forbidden to use RAM - only registers can be used.
+
+*/
+
     .text
     .global f
 
