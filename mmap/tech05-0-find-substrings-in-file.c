f0cef1818762a7d9aa3cc0e62d50515dd7d22439
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 09:57:17 +0300

    Create tech05-0-find-substrings-in-file.c

---
+++ mmap/tech05-0-find-substrings-in-file.c
@@ -0,0 +1,47 @@
+/*
+
+Программе передаются два аргумента: имя файла, и текстовая строка для поиска.
+
+Необходимо найти все вхождения строки в бинарном файле, используя отображение на память с помощью системного вызова mmap.
+
+На стандартный поток вывода вывести список всех позиций (с нуля) в файле, где втречаются строка.
+
+*/
+
+#define _GNU_SOURCE
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+
+    char* filename = argv[1];
+    char* substr = argv[2];
+
+    struct stat s;
+    int file = open(filename, O_RDONLY);
+    fstat(file, &s);
+
+    char* file_vir_mem = mmap(NULL, s.st_size, PROT_READ, MAP_PRIVATE, file, 0);
+
+    char* start = file_vir_mem;
+    const char* end = file_vir_mem + s.st_size;
+    size_t len_substr = strlen(substr);
+    
+    char* cur = start;
+
+    while ((cur = memmem(cur, end - cur, substr, len_substr)) != NULL) {
+        printf("%ld\n", cur - start);
+        ++cur;
+    }
+
+    munmap((void*)file_vir_mem, s.st_size);
+    close(file);
+
+    return 0;
+} 
