dd80c60ecce4d5a41dfa59c4753fbde4a83bf11c
DudeSoPErfect <72893727+DudeSoPErfect@users.noreply.github.com>
2023-02-21 09:29:49 +0300

    Create tech02-2-1-format-io-64.s

---
+++ asm-arm/tech02-2-1-format-io-64.s
@@ -0,0 +1,45 @@
+/*
+Реализуйте программу на языке ассемблера armv8 (AArch64), которая вычисляет сумму двух целых 32-битных десятичных чисел.
+
+Можно использовать функции стандартной библиотеки языка Си scanf и printf.
+
+В качестве точки входа можно считать функцию main.
+
+*/
+
+	.text
+	.global main
+
+main:
+	mov x20, x30
+	mov x19, x29
+
+	adr x0, int
+	adr x1, var1
+	adr x2, var2
+	bl scanf
+
+
+	adr x1, var1
+	ldr x1, [x1]
+	adr x2, var2
+        ldr x2, [x2]
+
+
+	add w1, w1, w2
+	adr x0, pr_int
+	bl printf
+
+
+	mov x30, x20
+	mov x29, x19
+	mov x0, 0
+	ret
+
+
+.data
+
+int:	.string "%d %d"
+pr_int:	.string "%d\n"
+var1:	.long 0
+var2:	.long 0
